012c97144997bd1d414c4e4d2dd8b62028736029
John Wu <z328wu@uwaterloo.ca>
2021-11-09 22:34:25 -0500

    inital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+
+function App() {
+  const name = "John";
+  const x = true;
+
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
